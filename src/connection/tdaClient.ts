df52ef44d43cc0181a322d82983db7fc3078b5bb
Phong Vuong <pyriter.io@gmail.com>
2021-12-28 20:55:22 -0800

    Added ability to get option chain

--- src/connection/tdaClient.ts
+++ src/connection/tdaClient.ts
@@ -1,15 +1,15 @@
 import { Interceptor } from './interceptor';
 import client from './client';
-import { getAccount } from '../api/accounts';
 import { PriceHistoryConfig } from '../models/priceHistory';
-import { getOptionChain } from '../api/OptionChains';
 import { getPriceHistory } from '../api/priceHistory';
+import { getAccount } from '../api/accounts';
 import { CredentialProvider } from '../providers/credentialsProvider';
-import { TdaClientBuilder } from './TdaClientBuilder';
+import { TdaClientBuilder } from './tdaClientBuilder';
 import { placeOrder } from '../api/orders';
 import { OrdersConfig, PlaceOrdersResponse } from '../models/order';
 import { AuthorizationTokenInterceptor } from './authorizationTokenInterceptor';
-import { OptionChainConfig } from '../models/optionChain';
+import { OptionChainConfig, OptionChainResponse } from '../models/optionChain';
+import { getOptionChain } from '../api/optionChain';
 
 export interface TdaClientConfig {
   authorizationInterceptor: Interceptor;
@@ -46,7 +46,7 @@ export class TdaClient {
     return await getPriceHistory(config);
   }
 
-  async getOptionChain(config: OptionChainConfig): Promise<any> {
+  async getOptionChain(config: OptionChainConfig): Promise<OptionChainResponse> {
     return await getOptionChain(config);
   }
 }
